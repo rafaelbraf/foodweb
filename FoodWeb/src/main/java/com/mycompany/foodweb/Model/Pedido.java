169f6d2b1084d7186e96c8abbcef17d49d6508d2
rafaelbraf <rafabrunofelix@hotmail.com>
2023-03-12 22:00:52 -0300

    refatora classe Pedido

--- FoodWeb/src/main/java/com/mycompany/foodweb/Model/Pedido.java
+++ FoodWeb/src/main/java/com/mycompany/foodweb/Model/Pedido.java
@@ -1,46 +1,39 @@
 package com.mycompany.foodweb.Model;
 
-import java.util.Date;
-
 public class Pedido {
     
-    int idPedido;
-    int idCliente;
+    Long id;
+    String dataHoraPedido;
+    String statusPedido;
     Restaurante restaurante;
-    String data;
-    String hora;
-    int idStatusPedido;
-    Double valorTotal;
-    String codigo;
 
     public Pedido() {
     }
 
-    public Pedido(int idPedido, int idCliente, Restaurante restaurante, String data, String hora, int idStatusPedido, Double valorTotal, String codigo) {
-        this.idPedido = idPedido;
-        this.idCliente = idCliente;
+    public Pedido(String dataHoraPedido, String statusPedido, Restaurante restaurante) {
+        this.dataHoraPedido = dataHoraPedido;
+        this.statusPedido = statusPedido;
         this.restaurante = restaurante;
-        this.data = data;
-        this.hora = hora;
-        this.idStatusPedido = idStatusPedido;
-        this.valorTotal = valorTotal;
-        this.codigo = codigo;
     }
 
-    public int getIdPedido() {
-        return idPedido;
+    public Long getId() {
+        return id;
     }
 
-    public void setIdPedido(int idPedido) {
-        this.idPedido = idPedido;
+    public String getDataHoraPedido() {
+        return dataHoraPedido;
     }
 
-    public int getIdCliente() {
-        return idCliente;
+    public void setDataHoraPedido(String dataHoraPedido) {
+        this.dataHoraPedido = dataHoraPedido;
     }
 
-    public void setIdCliente(int idCliente) {
-        this.idCliente = idCliente;
+    public String getStatusPedido() {
+        return statusPedido;
+    }
+
+    public void setStatusPedido(String statusPedido) {
+        this.statusPedido = statusPedido;
     }
 
     public Restaurante getRestaurante() {
@@ -49,46 +42,6 @@ public class Pedido {
 
     public void setRestaurante(Restaurante restaurante) {
         this.restaurante = restaurante;
-    }
-
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    public String getHora() {
-        return hora;
-    }
-
-    public void setHora(String hora) {
-        this.hora = hora;
-    }
-
-    public int getIdStatusPedido() {
-        return idStatusPedido;
-    }
-
-    public void setIdStatusPedido(int idStatusPedido) {
-        this.idStatusPedido = idStatusPedido;
-    }
-
-    public Double getValorTotal() {
-        return valorTotal;
-    }
-
-    public void setValorTotal(Double valorTotal) {
-        this.valorTotal = valorTotal;
-    }    
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
     }
     
 }
