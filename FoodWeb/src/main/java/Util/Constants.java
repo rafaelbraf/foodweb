d3bf874fc1710423ae6ecbebd79fe7db387383d4
rafaelbraf <rafabrunofelix@hotmail.com>
2023-04-08 14:17:42 -0300

    Adiciona url de requisição das Categorias em Constants

--- FoodWeb/src/main/java/Util/Constants.java
+++ FoodWeb/src/main/java/Util/Constants.java
@@ -3,5 +3,6 @@ package Util;
 public class Constants {
     
     public static final String BASE_URL_RESTAURANTES = "http://localhost:3001/restaurantes/";
+    public static final String BASE_URL_CATEGORIAS = "http://localhost:3001/categorias/";
     
 }
