c84d018715caa39e7e2d35a0be965a12977358aa
rafaelbraf <rafabrunofelix@hotmail.com>
2023-04-08 19:07:44 -0300

    Adiciona URL base do Produtos em Constants

--- FoodWeb/src/main/java/Util/Constants.java
+++ FoodWeb/src/main/java/Util/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
     
     public static final String BASE_URL_RESTAURANTES = "http://localhost:3001/restaurantes/";
     public static final String BASE_URL_CATEGORIAS = "http://localhost:3001/categorias/";
+    public static final String BASE_URL_PRODUTOS = "http://localhost:3001/produtos/";
     
 }
