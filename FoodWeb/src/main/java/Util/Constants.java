55e740775e2461b9bbf05b56fb2eb196e71c4f09
rafaelbraf <rafabrunofelix@hotmail.com>
2023-04-09 20:00:26 -0300

    Alteração em Constants

--- FoodWeb/src/main/java/Util/Constants.java
+++ FoodWeb/src/main/java/Util/Constants.java
@@ -2,6 +2,8 @@ package Util;
 
 public class Constants {
     
+    public static final String BASE_URL = "http://localhost:3001/";
+    
     public static final String BASE_URL_RESTAURANTES = "http://localhost:3001/restaurantes/";
     public static final String BASE_URL_CATEGORIAS = "http://localhost:3001/categorias/";
     public static final String BASE_URL_PRODUTOS = "http://localhost:3001/produtos/";
